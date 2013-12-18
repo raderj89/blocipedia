e3ba7a9da736385ab46ca751d3579e50e0d803e1
Jared Rader <raderj89@gmail.com>
2013-12-18 08:16:26 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Blocipedia::Application.routes.draw do
 
-  as :user do
-    get '/sign_up', to: 'users/registrations#new', as: :sign_up
-    get '/sign_in', to: 'devise/sessions#new', as: :sign_in
-    get '/:id/edit', to: 'users/registrations#edit', as: :edit
-  end
-
   devise_for :users, controllers: { registrations: 'users/registrations' }
   
   devise_scope :user do
+    get '/sign_up', to: 'users/registrations#new', as: :sign_up
+    get '/sign_in', to: 'devise/sessions#new', as: :sign_in
+    get '/:id/edit', to: 'users/registrations#edit', as: :edit
     put 'users/update_plan', :to => 'users/registrations#update_plan'
     put 'users/cancel_plan', :to => 'users/registrations#cancel_plan'
   end
